4460743ba3b599c9f838abdd84966a3eb9375c44
Kora Loudermilk <koraloudermilk@gmail.com>
2019-09-25 10:27:59 -0400

    center 404 text

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,14 +7,17 @@ import {Link} from 'gatsby';
 const h1Style = {
     color: ColorScheme.darkPurple
 }
+const center = {
+  textAlign: 'center'
+}
 
 export default () => (
 
   <Layout>
     <Header />
     <div id="indent">
-        <h1 style={h1Style}>This page doesnt exist!</h1>
-        <p>Go <Link to="" id="body">home</Link>.</p>
+        <h1 style={h1Style, center}>This page doesnt exist!</h1>
+        <p style={center}>Go <Link to="" id="body">home</Link>.</p>
     </div>
   </Layout>
 
